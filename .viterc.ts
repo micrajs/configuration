52d3110e01b9452e92309672b51c9e4c96dcffd9
Olavo Amorim Santos <olavo.a.santos@gmail.com>
2022-04-15 17:42:13 -0500

    chore: updated build configuration to make event emitted an external dependency

--- .viterc.ts
+++ .viterc.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from '@micra/vite-config/library';
 
 export default defineConfig({
-  plugins: [],
+  build: {
+    rollupOptions: {
+      external: ['@micra/event-emitter'],
+    },
+  },
 });
